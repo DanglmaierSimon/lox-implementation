6b20a5abdecd576ef7ff6fee5b53b9e65ff1a5aa
Simon Danglmaier <simon@danglmaier.dev>
2022-04-09 08:08:39 +0200

    Add IS_NATIVE and AS_NATIVE

--- cpplox/include/lox/objects/objnative.h
+++ cpplox/include/lox/objects/objnative.h
@@ -19,12 +19,15 @@ private:
   NativeFn _function = nullptr;
 };
 
-inline auto AS_NATIVE(Value value)
-{
-  return dynamic_cast<ObjNative*>(AS_OBJ(value))->function();
-}
-
 inline auto IS_NATIVE(Value value)
 {
   return isObjType(value, ObjType::NATIVE);
+}
+
+inline auto AS_NATIVE(Value value)
+{
+  assert(IS_NATIVE(value));
+  assert(dynamic_cast<ObjNative*>(AS_OBJ(value)) != nullptr);
+
+  return dynamic_cast<ObjNative*>(AS_OBJ(value))->function();
 }
