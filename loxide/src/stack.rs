1e0e7e41814680079a07dd236b43e30782d72ed1
Simon Danglmaier <simon@danglmaier.dev>
2023-10-30 12:25:51 +0100

    implement stack via vector

--- loxide/src/stack.rs
+++ loxide/src/stack.rs
@@ -1,49 +1,57 @@
 pub struct Stack<T, const SIZE: usize> {
-    data: [T; SIZE], // TODO: Change to MAybeUninit or use unsafe code
-    count: usize,
+    data: Vec<T>,
 }
 
-impl<T: std::default::Default + std::marker::Copy, const SIZE: usize> Stack<T, SIZE> {
+impl<T: Clone, const SIZE: usize> Stack<T, SIZE> {
     pub fn new() -> Self {
-        return Self {
-            data: [T::default(); SIZE],
-            count: 0,
-        };
+        return Self { data: vec![] };
     }
 
     pub fn count(&self) -> usize {
-        return self.count;
+        return self.data.len();
     }
 
-    pub fn capacity(&self) -> usize {
+    pub const fn capacity(&self) -> usize {
         return SIZE;
     }
 
     pub fn push(&mut self, val: T) {
         assert!(self.count() < SIZE);
-        self.data[self.count] = val;
-        self.count += 1;
+        self.data.push(val);
     }
 
     pub fn pop(&mut self) -> T {
         assert!(!self.empty());
-        self.count -= 1;
-        return self.data[self.count];
+        assert!(self.data.last().is_some());
+
+        return self.data.pop().unwrap();
     }
 
     pub fn top(&self) -> &T {
-        return &self.data[self.count() - 1];
+        assert!(!self.empty());
+        assert!(self.data.last().is_some());
+        self.check_invariants();
+        return self.data.last().unwrap();
     }
 
     pub fn empty(&self) -> bool {
+        self.check_invariants();
         return self.count() == 0;
     }
 
-    pub fn data(&self) -> &[T; SIZE] {
-        return &self.data;
+    pub fn data(&self) -> &[T] {
+        return self.data.as_slice();
     }
 
     pub fn peek(&self, idx: usize) -> &T {
-        return &self.data[self.count()-1-idx]
+        assert!(!self.empty());
+        assert!(self.count() > idx);
+        self.check_invariants();
+        return self.data.iter().rev().nth(idx).unwrap();
+    }
+
+    fn check_invariants(&self) {
+        // size of the vector may never exceed SIZE
+        assert!(self.data.len() <= self.capacity());
     }
 }
