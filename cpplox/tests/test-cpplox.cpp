6fb41b28d906cafda4e5943bc7e72f8321beaa64
Simon Danglmaier <simon@danglmaier.dev>
2023-11-08 13:57:36 +0100

    improve testing

--- cpplox/tests/test-cpplox.cpp
+++ cpplox/tests/test-cpplox.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <deque>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -11,14 +12,23 @@
 #include <string_view>
 #include <vector>
 
-#include <fmt/core.h>
 #include <fmt/format.h>
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "vm.h"
 
 using namespace std;
+
+namespace
+{
+const auto expectedOutputPattern = ("// expect: ?(.*)");
+const auto expectedErrorPattern = ("// (Error.*)");
+const auto errorLinePattern = ("// \\[((java|c) )?line (\\d+)\\] (Error.*)");
+const auto expectedRuntimeErrorPattern = ("// expect runtime error: (.+)");
+const auto syntaxErrorPattern = ("\\[line (\\d+)\\] (Error.+)");
+const auto stackTracePattern = ("\\[line (\\d+)\\]");
+const auto nonTestPattern = ("// nontest");
+}  // namespace
 
 struct RawTestOutput
 {
@@ -31,16 +41,15 @@ struct ExpectedTestResult
 {
   string file_name;
 
-  deque<string> expected_output;
+  // pair of line number and output
+  deque<pair<size_t, string>> expected_output;
 
   // compiler errors
   deque<string> expected_errors;
 
   optional<string> expected_runtime_error;
 
-  int expected_runtime_error_line;
-
-  int expected_exit_code;
+  size_t expected_runtime_error_line;
 };
 
 deque<string> split_newlines(string output)
@@ -78,22 +87,9 @@ RawTestOutput run_impl(string source)
                         split_newlines(stderrstream.str())};
 }
 
-deque<string> stripDebugOutput(deque<std::string> in)
-{
-  deque<string> res;
-
-  for (auto str : in) {
-    if (!(str.rfind("DBG:", 0) == 0)) {
-      res.push_back(str);
-    }
-  }
-
-  return res;
-}
-
 optional<string> parse_expected_output(string line)
 {
-  regex rgx {"// *expect: *(.*)$"};
+  regex rgx {expectedOutputPattern};
   smatch match;
 
   if (regex_search(line.cbegin(), line.cend(), match, rgx)) {
@@ -105,28 +101,35 @@ optional<string> parse_expected_output(string line)
 
 optional<string> parse_runtime_error(string line)
 {
-  // expect runtime error: Undefined variable 'unknown'.
-
-  std::regex rgx {"// *expect runtime error: *(.*)"};
+  std::regex rgx {expectedRuntimeErrorPattern};
   std::smatch match;
 
   if (std::regex_search(line.cbegin(), line.cend(), match, rgx)) {
-    return RuntimeError {match[1]};
+    return {match[1]};
   }
 
   return std::nullopt;
 }
 
-optional<CompileError> parse_compile_error(string line)
+optional<string> parse_error_pattern(string line)
 {
-  std::regex rgx {"// ?(.*)? (Error.*)"};  // pattern for any compile error
+  std::regex rgx {expectedErrorPattern};
   std::smatch match;
 
   if (std::regex_search(line.cbegin(), line.cend(), match, rgx)) {
-    const string lineNr = match[1];
-    const string what = match[2];
+    return std::string {match[1]};
+  }
 
-    return CompileError {std::atoi(lineNr.c_str()), what};
+  return nullopt;
+}
+
+optional<string> parse_error_line_pattern(string line)
+{
+  std::regex rgx {errorLinePattern};
+  std::smatch match;
+
+  if (std::regex_search(line.cbegin(), line.cend(), match, rgx)) {
+    return fmt::format("[line {}] {}", string {match[3]}, string {match[4]});
   }
 
   return nullopt;
@@ -138,215 +141,249 @@ ExpectedTestResult parse_expected_output_from_lox_source(string lox_source)
 
   auto lines = split_newlines(lox_source);
 
-  bool found_runtime_error = false;
-  bool found_compile_error = false;
-
   for (size_t linenr = 1; linenr <= lines.size(); linenr++) {
     const auto line = lines.at(linenr - 1);
 
     // check line for output
     if (auto output = parse_expected_output(line)) {
-      EXPECT_FALSE(found_compile_error)
-          << "cannot parse output after already having found a compiler or "
-             "runtime error";
-      EXPECT_FALSE(found_runtime_error)
-          << "cannot parse output after already having found a compiler or "
-             "runtime error";
+      result.expected_output.push_back(pair {linenr, *output});
+      continue;
+    }
 
-      cout << "found expected output at line " << to_string(linenr) << endl;
-      result.output.push_back(*output);
-    } else if (auto runtime_error = parse_runtime_error(line)) {
-      EXPECT_FALSE(found_compile_error)
-          << "only 1 compile or runtime error may be found per test";
-      EXPECT_FALSE(found_runtime_error)
-          << "only 1 compile or runtime error may be found per test";
+    // check line for errors
+    if (auto compile_error = parse_error_pattern(line)) {
+      result.expected_errors.push_back(
+          fmt::format("[line {}] {}", linenr, *compile_error));
 
-      cout << "found expected runtime error at line " << to_string(linenr)
-           << endl;
-      result.rt_error = runtime_error;
-      found_runtime_error = true;
-    } else if (auto compile_error = parse_compile_error(line)) {
-      EXPECT_FALSE(found_compile_error)
-          << "only 1 compile or runtime error may be found per test";
-      EXPECT_FALSE(found_runtime_error)
-          << "only 1 compile or runtime error may be found per test";
+      continue;
+    }
 
-      cout << "found expected compile time error at line " << to_string(linenr)
-           << endl;
-      result.ct_error = compile_error;
-      found_compile_error = true;
-    } else {
-      if (line.find("Error") != string::npos) {
-        cout << "found line with error string, but not caught in above "
-                "checks!"
-             << endl;
-        cout << "line: " << line << endl
-             << "line nr: " << to_string(linenr) << endl;
-      }
+    if (auto match = parse_error_line_pattern(line)) {
+      result.expected_errors.push_back(*match);
+      continue;
+    }
+
+    if (auto runtime_error = parse_runtime_error(line)) {
+      EXPECT_FALSE(result.expected_runtime_error.has_value());
+      result.expected_runtime_error = runtime_error;
+      result.expected_runtime_error_line = linenr;
     }
   }
 
   return result;
 }
 
-optional<string> parse_actual_output(string line)
-{
-  if (line.empty()) {
-    return nullopt;
-  }
+// optional<string> parse_actual_output(string line)
+// {
+//   if (line.empty()) {
+//     return nullopt;
+//   }
 
-  return line;
+//   return line;
+// }
+
+// optional<string> parse_actual_runtime_error(string prev_line, string line)
+// {
+//   std::regex rgx {"line (.*)] in (.*)"};
+//   std::smatch match;
+//   if (std::regex_search(line.cbegin(), line.cend(), match, rgx)) {
+//     const string linenr = match[1];
+//     const string in = match[2];
+
+//     return {prev_line};
+//   }
+
+//   return nullopt;
+// }
+
+// optional<string> parse_actual_compile_error(string line)
+// {
+//   std::regex rgx {"(.*)? ?(Error.*)"};
+//   std::smatch match;
+//   if (std::regex_search(line.cbegin(), line.cend(), match, rgx)) {
+//     const string lineNr = match[1];
+//     const string what = match[2];
+
+//     return {what};
+//   }
+
+//   return nullopt;
+// }
+
+/*
+final _expectedOutputPattern = RegExp(r"// expect: ?(.*)");
+final _expectedErrorPattern = RegExp(r"// (Error.*)");
+final _errorLinePattern = RegExp(r"// \[((java|c) )?line (\d+)\] (Error.*)");
+final _expectedRuntimeErrorPattern = RegExp(r"// expect runtime error: (.+)");
+final _syntaxErrorPattern = RegExp(r"\[.*line (\d+)\] (Error.+)");
+final _stackTracePattern = RegExp(r"\[line (\d+)\]");
+final _nonTestPattern = RegExp(r"// nontest");
+*/
+
+void validate_runtime_error(RawTestOutput output, ExpectedTestResult expected)
+{
+  auto errorlines = output.stderrOutput;
+
+  ASSERT_TRUE(expected.expected_runtime_error.has_value());
+
+  auto expected_runtime_error = *expected.expected_runtime_error;
+
+  ASSERT_GE(errorlines.size(), 2) << fmt::format(
+      "Expected runtime error {} and got none.", expected_runtime_error);
+
+  ASSERT_EQ(expected_runtime_error, errorlines[0])
+      << fmt::format("Expected runtime error '{}' and got '{}'.",
+                     expected_runtime_error,
+                     errorlines[0]);
+
+  // TODO: parse stacktrace and verify
+  // * correct stack trace pattern
+  // * line numbers of stacktrace match
 }
 
-optional<RuntimeError> parse_actual_runtime_error(string prev_line, string line)
+bool contains(const deque<string>& haystack, const string& needle)
 {
-  std::regex rgx {"line (.*)] in (.*)"};
-  std::smatch match;
-  if (std::regex_search(line.cbegin(), line.cend(), match, rgx)) {
-    const string linenr = match[1];
-    const string in = match[2];
+  auto it = find(cbegin(haystack), cend(haystack), needle);
 
-    return RuntimeError {prev_line};
+  if (it == cend(haystack)) {
+    return false;
   }
 
-  return nullopt;
+  return true;
 }
 
-optional<CompileError> parse_actual_compile_error(string line)
+deque<string> difference(deque<string> lhs, deque<string> rhs)
 {
-  std::regex rgx {"(.*)? ?(Error.*)"};
-  std::smatch match;
-  if (std::regex_search(line.cbegin(), line.cend(), match, rgx)) {
-    const string lineNr = match[1];
-    const string what = match[2];
+  deque<string> res;
+  copy_if(cbegin(lhs),
+          cend(lhs),
+          back_inserter(res),
+          [&res, &rhs](const string& elem) {
+            if (contains(res, elem)) {
+              return false;
+            }
 
-    return CompileError {atoi(lineNr.c_str()), what};
+            if (contains(rhs, elem)) {
+              return false;
+            }
+            return true;
+          });
+
+  copy_if(cbegin(rhs),
+          cend(rhs),
+          back_inserter(res),
+          [&res, &lhs](const string& elem) {
+            if (contains(res, elem)) {
+              return false;
+            }
+
+            if (contains(lhs, elem)) {
+              return false;
+            }
+            return true;
+          });
+
+  return res;
+}
+
+void validate_compile_error(RawTestOutput output, ExpectedTestResult expected)
+{
+  // Validate that every compile error was expected.
+  deque<string> found_errors;
+  int unexpected_count = 0;
+
+  for (string line : output.stderrOutput) {
+    std::regex rgx {syntaxErrorPattern};
+    std::smatch match;
+
+    if (std::regex_search(line, match, rgx)) {
+      const string lineNr = match[1];
+      const string what = match[2];
+
+      auto error = fmt::format("[line {}] {}", lineNr, what);
+
+      if (contains(expected.expected_errors, error)) {
+        found_errors.push_back(error);
+      } else {
+        if (unexpected_count < 10) {
+          ADD_FAILURE() << fmt::format("Unexpected error: {}", line);
+        }
+
+        unexpected_count++;
+      }
+
+    } else if (line != "") {
+      if (unexpected_count < 10) {
+        ADD_FAILURE() << fmt::format("Unexpected output on stderr: {}", line);
+      }
+
+      unexpected_count++;
+    }
   }
 
-  return nullopt;
+  if (unexpected_count > 10) {
+    ADD_FAILURE() << fmt::format("truncated {} more...", unexpected_count - 10);
+  }
+
+  // Validate that every expected error occurred.
+  auto diff = difference(expected.expected_errors, found_errors);
+  for (auto error : diff) {
+    ADD_FAILURE() << fmt::format("Missing expected error: {}", error);
+  }
 }
 
-void validate_runtime_error() {}
+void validate_output(RawTestOutput output, ExpectedTestResult expected)
+{
+  auto output_lines = output.stdoutOutput;
 
-void validate_compile_error() {}
+  if (!output_lines.empty() && output_lines.back() == "") {
+    // remove trailing empty line
+    output_lines.pop_back();
+  }
 
-void validate_exit_code() {}
+  auto index = 0u;
 
-void validate_output() {}
+  for (; index < output_lines.size(); index++) {
+    string line = output_lines[index];
 
-void check_results(ExpectedTestResult expected,
-                   RawTestOutput actual,
-                   string file_name)
+    ASSERT_LT(index, expected.expected_output.size());
+
+    auto exp = expected.expected_output[index];
+
+    EXPECT_EQ(exp.second, line);
+  }
+
+  while (index < expected.expected_output.size()) {
+    auto exp = expected.expected_output[index];
+
+    ADD_FAILURE() << fmt::format(
+        "Missing expected output '{}' on line {}", exp.second, exp.first);
+    index++;
+  }
+}
+
+void check_results(ExpectedTestResult expected, RawTestOutput output)
 {
   // runtime and compile time error may not happen at the same time
-  ASSERT_FALSE(expected.rt_error.has_value() && expected.ct_error.has_value());
+  ASSERT_FALSE(expected.expected_runtime_error.has_value()
+               && !expected.expected_errors.empty());
 
-  if (expected.rt_error.has_value()) {
-    validate_runtime_error();
-  } else if (expected.ct_error.has_value()) {
-    validate_compile_error();
+  if (expected.expected_runtime_error.has_value()) {
+    validate_runtime_error(output, expected);
+  } else {
+    validate_compile_error(output, expected);
   }
 
-  validate_exit_code();
-  validate_output();
-
-  while (true) {
-    if (actual_deque.empty() || expected_deque.empty()) {
-      break;
-    }
-
-    const auto act = actual_deque.front();
-    const auto exp = expected_deque.front();
-
-    actual_deque.pop_front();
-    expected_deque.pop_front();
-
-    EXPECT_EQ(act, exp);
-  }
-
-  if ((actual_deque.empty() && expected_deque.empty())) {
-    return;
-  }
-
-  // drain all the non-received expected values
-  while (true) {
-    if (expected_deque.empty()) {
-      break;
-    }
-
-    const auto exp = expected_deque.front();
-
-    expected_deque.pop_front();
-
-    ADD_FAILURE() << "Never received expected output " << exp.to_string();
-  }
-
-  while (true) {
-    if (actual_deque.empty()) {
-      break;
-    }
-
-    const auto act = actual_deque.front();
-
-    actual_deque.pop_front();
-
-    if (act.type == OutputType::output
-        && file_name.find("benchmark") != string::npos)
-    {
-      // its a benchmark -> ignore unexpected output
-      continue;
-    }
-
-    ADD_FAILURE() << "Received unexpected output " << act.to_string();
-  }
-
-  cout << "=========================================" << endl;
-  cout << "STDOUT: " << endl << out << endl;
-  cout << "=========================================" << endl;
-  cout << "STDERR: " << endl << err << endl;
-  cout << "=========================================" << endl;
+  validate_output(output, expected);
 }
 
-void check_for_compile_and_runtime_error(vector<TestOutput> expected)
+void run(string source)
 {
-  const bool compile_errors =
-      count_if(cbegin(expected), cend(expected), [](TestOutput out) {
-        return out.type == OutputType::compile_error;
-      });
-
-  const bool has_compile_error = compile_errors > 0;
-
-  const bool runtime_errors =
-      count_if(cbegin(expected), cend(expected), [](TestOutput out) {
-        return out.type == OutputType::runtime_error;
-      });
-
-  const bool has_runtime_error = runtime_errors > 0;
-
-  ASSERT_TRUE(runtime_errors == 0 || runtime_errors == 1);
-
-  ASSERT_FALSE(has_compile_error && has_runtime_error)
-      << "runtime error and compile time error may not exist at the same "
-         "time";
-}
-
-void run(string file_name, string source)
-{
-  (void)file_name;
   const auto expected_output = parse_expected_output_from_lox_source(source);
-
-  check_for_compile_and_runtime_error(expected_output);
 
   const auto res = run_impl(source);
 
-  const auto actual_results =
-      parse_actual_results(res.stderrOutput, res.stdoutOutput);
-
-  check_results(expected_output,
-                actual_results,
-                res.stderrOutput,
-                res.stdoutOutput,
-                file_name);
+  check_results(expected_output, res);
 }
 
 class LoxTextFixture : public testing::Test
@@ -397,11 +434,17 @@ public:
 
     ASSERT_TRUE(source.has_value());
 
-    run(m_file_path, *source);
+    run(*source);
   }
 
 private:
   string m_file_path;
+};
+
+struct TestData
+{
+  string test_suite;
+  string test_name;
 };
 
 void register_tests(const vector<string>& paths)
@@ -411,8 +454,8 @@ void register_tests(const vector<string>& paths)
         "MyFixture",
         ("Test" + (path)).c_str(),
         nullptr,
+        nullptr,
         (path).c_str(),
-        __FILE__,
         __LINE__,
         // Important to use the fixture type as the return type here.
         [=]() -> LoxTextFixture* { return new LoxTest(path); });
@@ -423,9 +466,13 @@ vector<string> get_all_tests()
 {
   vector<string> ret;
 
-  auto current = "test";
+  for (auto entry : filesystem::recursive_directory_iterator("test")) {
+    auto str = entry.path().string();
 
-  for (auto entry : filesystem::recursive_directory_iterator(current)) {
+    if (str.find("benchmark") != string::npos) {
+      continue;
+    }
+
     if (entry.is_regular_file()) {
       ret.push_back(entry.path());
     }
