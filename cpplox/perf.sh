b5eef036cd4855af09f6615ea0ae85d5f4e962a2
Simon Danglmaier <simon@danglmaier.dev>
2023-11-09 10:09:34 +0100

    update perf.sh

--- cpplox/perf.sh
+++ cpplox/perf.sh
@@ -2,8 +2,26 @@
 
 set -euo pipefail
 
+echo "configure"
+
 cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebug -G Ninja
+
+echo "build"
 
 cmake --build build
 
-perf record build/src/cpplox "$(pwd)/tests/test/benchmark/equality.lox"
+echo "record"
+
+# perf record -o /home/simon/playground/lox-implementation/cpplox/perf.data --call-graph dwarf --aio --sample-cpu /home/simon/playground/lox-implementation/cpplox/build/src/cpplox /home/simon/playground/lox-implementation/cpplox/tests/test/benchmark/equality.lox
+
+# perf record build/src/cpplox "$(pwd)/tests/test/benchmark/equality.lox"
+
+for f in "$(pwd)"/tests/test/benchmark/*.lox; do
+    echo "running benchmark $(basename "$f")"
+
+    OUTPUT_FILE="$(basename "$f").perf.data"
+    BINARY=$(pwd)/build/src/cpplox
+
+    perf record -o "${OUTPUT_FILE}" --call-graph dwarf --aio --sample-cpu "${BINARY}" "$f"
+
+done
