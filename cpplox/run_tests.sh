f6080531d4d3af6329b09f961502cd4fe9121b82
Simon Danglmaier <simon@danglmaier.dev>
2024-10-01 21:47:38 +0200

    Add test scripts

---
+++ cpplox/run_tests.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BUILD_CONFIGS="Debug Release"
+SANITIZERS="none address undefined" 
+
+
+for CFG in $BUILD_CONFIGS; do
+    for SAN in $SANITIZERS; do
+        echo "configuring with config ${CFG} and sanitizer ${SAN}..."
+
+        if [[ "$SAN" == "none" ]]; then
+             cmake -G Ninja \
+                 -DCMAKE_CXX_COMPILER=clang++ \
+                 -DCMAKE_C_COMPILER=clang \
+                 -DCMAKE_BUILD_TYPE="${CFG}" \
+                 . -B build
+
+            echo "building..."
+            cmake --build build
+            echo "testing..."
+            python3 test.py build/cpplox
+            echo "============================"
+        else
+             cmake -G Ninja \
+                 -DCMAKE_CXX_COMPILER=clang++ \
+                 -DCMAKE_C_COMPILER=clang \
+                 -DCMAKE_BUILD_TYPE="${CFG}" \
+                 -DCMAKE_CXX_FLAGS=-fsanitize="${SAN}" \
+                 -DCMAKE_EXE_LINKER_FLAGS=-fsanitize="${SAN}" . -B build/"${CFG}"/"${SAN}"
+
+            echo "building..."
+            cmake --build build/"${CFG}"/"${SAN}"
+            echo "testing..."
+            python3 test.py build/"${CFG}"/"${SAN}"/cpplox
+            echo "============================"
+        fi
+
+    done
+
+    echo "============================"
+done
